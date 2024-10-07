6d4ec48b2731fde094833f759f5eb8db937b7523
Aashish1-1-1 <aashishadhikari693@gmail.com>
2024-10-07 10:43:16 +0545

    [update: RandomLinkChain]

--- BlockChain/blockchain.go
+++ BlockChain/blockchain.go
@@ -1,49 +1,49 @@
 package BlockChain
 
-import(
-	"fmt"
+import (
 	"crypto/sha256"
+	"encoding/json"
+	"fmt"
 	"strconv"
 	"strings"
 	"time"
-	"encoding/json"
 )
 
-type Block struct{
-	data map[string]interface{}
-	hash  string
+type Block struct {
+	data         map[string]interface{}
+	hash         string
 	previousHash string
-	timestamp time.Time
-	pow int
+	timestamp    time.Time
+	pow          int
 }
 
-type Blockchain struct{
-	genesis Block
-	chain []Block
+type Blockchain struct {
+	genesis    Block
+	chain      []Block
 	difficulty int
 }
-type Jsondata struct{
-	Platform string `json:platform`
-	Link interface{} `json:link`
+type Jsondata struct {
+	Title string `json:title`
+	Link  string `json:link`
 }
 
-func (b Block) calculateHash()string{
-	data, _:=json.Marshal(b.data)
+func (b Block) calculateHash() string {
+	data, _ := json.Marshal(b.data)
 	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
 	blockHash := sha256.Sum256([]byte(blockData))
 	return fmt.Sprintf("%x", blockHash)
 }
 
-func (b *Block) mine(difficulty int){
-	for !strings.HasPrefix(b.hash, strings.Repeat("0",difficulty)){
+func (b *Block) mine(difficulty int) {
+	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
 		b.pow++
-		b.hash=b.calculateHash()
+		b.hash = b.calculateHash()
 	}
 }
 
-func CreateBlockchain(difficulty int) Blockchain{
-	genesisBlock:=Block{
-		hash: "0",
+func CreateBlockchain(difficulty int) Blockchain {
+	genesisBlock := Block{
+		hash:      "0",
 		timestamp: time.Now(),
 	}
 	return Blockchain{
@@ -53,26 +53,26 @@ func CreateBlockchain(difficulty int) Blockchain{
 	}
 }
 
-func (b *Blockchain) AddBlock(platform string,Link string){
-	blockData :=map[string]interface{}{
-		"Platform": platform,
-		"Link": Link,
+func (b *Blockchain) AddBlock(Title string, Link string) {
+	blockData := map[string]interface{}{
+		"Title": Title,
+		"Link":  Link,
 	}
 	lastBlock := b.chain[len(b.chain)-1]
 	newBlock := Block{
-		data: blockData,
+		data:         blockData,
 		previousHash: lastBlock.hash,
-		timestamp: time.Now(),
+		timestamp:    time.Now(),
 	}
 	newBlock.mine(b.difficulty)
-	b.chain = append(b.chain,newBlock)
+	b.chain = append(b.chain, newBlock)
 }
 
-func (b Blockchain) IsValid() bool{
-	for i:=range b.chain[1:]{
-		previousBlock:=b.chain[i]
-		currentBlock:=b.chain[i+1]
-		if currentBlock.hash != currentBlock.calculateHash()||currentBlock.previousHash != previousBlock.hash {
+func (b Blockchain) IsValid() bool {
+	for i := range b.chain[1:] {
+		previousBlock := b.chain[i]
+		currentBlock := b.chain[i+1]
+		if currentBlock.hash != currentBlock.calculateHash() || currentBlock.previousHash != previousBlock.hash {
 			return false
 		}
 	}
@@ -81,24 +81,15 @@ func (b Blockchain) IsValid() bool{
 func (b Blockchain) GetBlockchainData() []Jsondata {
 	var jsonData []Jsondata
 	for _, block := range b.chain {
-		platform := ""
+		Title := ""
 		link := ""
-		if platformVal, ok := block.data["Platform"]; ok {
-			platform = platformVal.(string)
+		if TitleVal, ok := block.data["Title"]; ok {
+			Title = TitleVal.(string)
 		}
 		if linkVal, ok := block.data["Link"]; ok {
 			link = linkVal.(string)
 		}
-		jsonData = append(jsonData, Jsondata{Platform: platform, Link: link})
+		jsonData = append(jsonData, Jsondata{Title: Title, Link: link})
 	}
 	return jsonData
 }
-//func main(){
-//	blockChain:= CreateBlockchain(3)
-//	blockChain.addBlock("Github","https://github.com/Aashish1-1-1")
-//	blockChain.addBlock("Linkedln","https://linkedln.com/Aashish1-1-1")
-//	blockChain.addBlock("Instagram","https://instagram.com/Blessadhikari")
-//	blockChain.addBlock("FaceBook","https://facebook.com/Blessadhikari")
-//	blockChain.printBlockchain()
-//	fmt.Println(blockChain.isValid())
-//}
